b8be95354836440f2cd191f7e16ffe4c3b180f4b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-09-28 11:26:16 +0300

    chore(app): configure ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`,
+        }),
+        UsersModule,
+    ],
 })
 export class AppModule {}
