86d9c2ce2465adf6c59a7cf7a25ebc07652c69af
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-09-29 23:26:41 +0300

    feat(app): integrate ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 import { AuthModule } from './auth/auth.module';
+import { FilesModule } from './files/files.module';
+import { Post } from './posts/posts.model';
+import { PostsModule } from './posts/posts.module';
 import { Role } from './roles/roles.model';
 import { RolesModule } from './roles/roles.module';
 import { UserRoles } from './roles/user-roles.model';
 import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
-import { Post } from './posts/posts.model';
-import { FilesModule } from './files/files.module';
 
 @Module({
     controllers: [],
@@ -17,6 +19,9 @@ import { FilesModule } from './files/files.module';
     imports: [
         ConfigModule.forRoot({
             envFilePath: `.${process.env.NODE_ENV}.env`,
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static'),
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
