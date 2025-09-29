e44d531066bcb06068a404c1a12f272bbf98c3b4
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-09-29 23:24:19 +0300

    feat(app): integrate post and file modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { RolesModule } from './roles/roles.module';
 import { UserRoles } from './roles/user-roles.model';
 import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { Post } from './posts/posts.model';
+import { FilesModule } from './files/files.module';
 
 @Module({
     controllers: [],
@@ -22,12 +25,14 @@ import { UsersModule } from './users/users.module';
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role, UserRoles],
+            models: [User, Role, UserRoles, Post],
             autoLoadModels: true,
         }),
         UsersModule,
         RolesModule,
         AuthModule,
+        PostsModule,
+        FilesModule,
     ],
 })
 export class AppModule {}
