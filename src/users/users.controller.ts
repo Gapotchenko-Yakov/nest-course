11b320cd71d13e6d65eb022010d46df0fe5cdb87
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-09-29 20:56:39 +0300

    feat(user): implement addRole and ban endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import { RolesGuard } from 'src/auth/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
-    constructor(private userService: UsersService) { }
+    constructor(private userService: UsersService) {}
 
     @ApiOperation({ summary: 'Создание пользователя' })
     @ApiResponse({ status: 200, type: User })
@@ -26,5 +28,23 @@ export class UsersController {
     @Get()
     getAll() {
         return this.userService.getAllUsers();
+    }
+
+    @ApiOperation({ summary: 'Выдать роль' })
+    @ApiResponse({ status: 200 })
+    @Roles('ADMIN')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.userService.addRole(dto);
+    }
+
+    @ApiOperation({ summary: 'Забанить пользователя' })
+    @ApiResponse({ status: 200 })
+    @Roles('ADMIN')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.userService.ban(dto);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
-import { RolesService } from 'src/roles/roles.service';
 
 @Injectable()
 export class UsersService {
@@ -35,5 +37,34 @@ export class UsersService {
             include: { all: true },
         });
         return user;
+    }
+
+    async addRole(dto: AddRoleDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        const role = await this.roleService.getRoleByValue(dto.value);
+        if (role && user) {
+            await user?.$add('role', role.id);
+            return user.get({ plain: true });
+        }
+        throw new HttpException(
+            'Пользователь или роль не найдены',
+            HttpStatus.NOT_FOUND,
+        );
+    }
+
+    async ban(dto: BanUserDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        if (!user) {
+            throw new HttpException(
+                'Пользователь не найден',
+                HttpStatus.NOT_FOUND,
+            );
+        }
+        user.set({
+            banned: true,
+            banReason: dto.banReason,
+        });
+        await user.save();
+        return user.get({ plain: true });
     }
 }
